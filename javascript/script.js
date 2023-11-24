5ddd0396bef757d2eb27ce2213541ccdae66d849
Prosper Ikechukwu <mrhendrix62@gmail.com>
2023-11-24 10:40:54 +0100

    FIX: Pushing online to aid CI/CD

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const navLinks = document.querySelectorAll('.links');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+mainMenu.addEventListener('click', function (e) {
+	if (e.target.className == 'links') {
+		mainMenu.classList.add('hide');
+	}
+});
+
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+function close() {
+  mainMenu.style.top = '-105%';
+}
