a67c21fbac4cb2ddd9d4d1f097d15cd58245dafe
Prosper Ikechukwu <mrhendrix62@gmail.com>
2023-11-24 16:30:04 +0100

    FEAT: Completed Validation with JavaScript

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,10 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const navLinks = document.querySelectorAll('.links');
+const form = document.querySelector('#form');
+const inputName = document.querySelector('#name');
+const inputEmail = document.querySelector('#email')
+const inputFlower = document.querySelector('#flowerName');
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
@@ -18,4 +22,29 @@ function show() {
 }
 function close() {
   mainMenu.style.top = '-105%';
+}
+
+form.addEventListener('submit', validate);
+
+function validate(e) {
+  e.preventDefault(); 
+  
+  if (inputName.value == '') {
+    alert('Please fill in a valid name')
+    return
+  }
+
+  if (inputEmail.value == '') {
+		alert('Please fill in a valid email address');
+    return
+  }
+
+  if (inputFlower.value == '') {
+    alert('Please fill in the flower name that you\'ll like to order')
+    return
+  }
+ 
+  if (inputName.value != '' && inputEmail.value != '' && inputFlower.value != '') {
+    alert(`Thanks ${inputName.value} for contacting us about ${inputFlower.value} \n We'll get back to you via your email`)
+  }
 }
